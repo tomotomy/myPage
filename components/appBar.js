25965f3c244d9b98acf113790c6427459f59804c
tomotomy <tomo1999724@gmail.com>
2020-08-26 18:51:00 +0900

    first commit

---
+++ components/appBar.js
@@ -0,0 +1,17 @@
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from '@material-ui/core'
+
+export default function Header() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+    
+      </Toolbar>
+    </AppBar>
+  )
+}
